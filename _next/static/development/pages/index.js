dee8b05cd3fac5a4c38ae23ca9f694ebc4385ed5
magnonta <magnonta@gmail.com>
2019-06-18 13:53:51 -0300

    Deploy to gh-pages

--- _next/static/development/pages/index.js
+++ _next/static/development/pages/index.js
@@ -12296,64 +12296,114 @@ function (_React$Component) {
         firstSection: true,
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 42
+          lineNumber: 46
         },
         __self: this
       }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("p", {
-        __source: {
-          fileName: _jsxFileName,
-          lineNumber: 43
-        },
-        __self: this
-      }, "Cont\xE9udo com os palestrantes")), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(_components_mini_section__WEBPACK_IMPORTED_MODULE_10__["default"], {
-        title: "Inscri\xE7\xF5es",
-        id: "inscription",
-        alignCenter: true,
-        __source: {
-          fileName: _jsxFileName,
-          lineNumber: 47
-        },
-        __self: this
-      }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("br", {
-        __source: {
-          fileName: _jsxFileName,
-          lineNumber: 48
-        },
-        __self: this
-      }), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("br", {
-        __source: {
-          fileName: _jsxFileName,
-          lineNumber: 48
-        },
-        __self: this
-      }), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("div", {
         __source: {
           fileName: _jsxFileName,
           lineNumber: 49
         },
         __self: this
-      }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("p", {
+      }, "Cont\xE9udo com os palestrantes")), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(_components_mini_section__WEBPACK_IMPORTED_MODULE_10__["default"], {
+        title: "Programa\xE7\xE3o",
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 50
+          lineNumber: 53
         },
         __self: this
-      }, "As inscri\xE7\xF5es est\xE3o dispon\xEDveis no per\xEDodo de 17/06/2019 a 17/08/2019"), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("p", {
+      }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("br", {
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 51
+          lineNumber: 54
         },
         __self: this
-      }, "\xC9 necess\xE1rio um 1kg de alimento para ter acesso ao evento que ser\xE1 doado!"), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("br", {
+      }), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("div", {
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 51
+          lineNumber: 55
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("ul", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 56
+        },
+        __self: this
+      }, _utils_config__WEBPACK_IMPORTED_MODULE_15__["default"].programacao.map(function (p) {
+        return react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("li", {
+          __source: {
+            fileName: _jsxFileName,
+            lineNumber: 58
+          },
+          __self: this
+        }, "".concat(p.horario, " - ").concat(p.nome));
+      }))), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("br", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 62
+        },
+        __self: this
+      })), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(_components_section__WEBPACK_IMPORTED_MODULE_9__["default"], {
+        title: "Patrocinadores",
+        backgroundImage: "url('".concat(BACKEND_URL, "/static/img/section-background/dark-background.png')"),
+        backdrop: true,
+        alignCenter: true,
+        firstSection: true,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 65
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(_components_mini_section__WEBPACK_IMPORTED_MODULE_10__["default"], {
+        title: "Inscri\xE7\xF5es",
+        id: "inscription",
+        alignCenter: true,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 70
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("br", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 71
         },
         __self: this
       }), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("br", {
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 52
+          lineNumber: 71
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("div", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 72
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("p", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 73
+        },
+        __self: this
+      }, "As inscri\xE7\xF5es est\xE3o dispon\xEDveis no per\xEDodo de 17/06/2019 a 17/08/2019"), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("p", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 74
+        },
+        __self: this
+      }, "\xC9 necess\xE1rio um 1kg de alimento para ter acesso ao evento que ser\xE1 doado!"), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("br", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 74
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("br", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 75
         },
         __self: this
       }), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("a", {
@@ -12362,7 +12412,7 @@ function (_React$Component) {
         target: "_blank",
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 53
+          lineNumber: 76
         },
         __self: this
       }, "Link de inscri\xE7\xE3o"))), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(_components_mini_section__WEBPACK_IMPORTED_MODULE_10__["default"], {
@@ -12370,7 +12420,7 @@ function (_React$Component) {
         backgroundImage: "url('".concat(BACKEND_URL, "/static/img/section-background/dark-background.png')"),
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 56
+          lineNumber: 80
         },
         __self: this
       }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(react_animate_on_scroll__WEBPACK_IMPORTED_MODULE_8___default.a, {
@@ -12378,50 +12428,102 @@ function (_React$Component) {
         duration: 2,
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 57
+          lineNumber: 82
         },
         __self: this
       }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("img", {
-        src: "".concat(BACKEND_URL, "/static/img/icons/twitter-white.png"),
-        alt: "twitter",
+        src: "".concat(BACKEND_URL, "/static/img/icons/instagram_white.png"),
+        alt: "instagram",
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 57
+          lineNumber: 82
         },
         __self: this
       }), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("br", {
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 57
+          lineNumber: 84
         },
         __self: this
       })), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("h1", {
         className: "follow-twitter",
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 58
+          lineNumber: 85
         },
         __self: this
       }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("a", {
-        href: "https://twitter.com/search?q=%23".concat(_utils_config__WEBPACK_IMPORTED_MODULE_15__["default"].hashTag),
+        href: "https://www.instagram.com/".concat(_utils_config__WEBPACK_IMPORTED_MODULE_15__["default"].hashTag),
         target: "_blank",
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 58
+          lineNumber: 85
         },
         __self: this
       }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(_components_animated_text__WEBPACK_IMPORTED_MODULE_14__["default"], {
-        text: _utils_config__WEBPACK_IMPORTED_MODULE_15__["default"].hashTag,
+        text: '@' + _utils_config__WEBPACK_IMPORTED_MODULE_15__["default"].hashTag,
         className: "text-styled",
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 59
+          lineNumber: 87
         },
         __self: this
-      })))), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(_components_footer__WEBPACK_IMPORTED_MODULE_13__["default"], {
+      })))), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(_components_mini_section__WEBPACK_IMPORTED_MODULE_10__["default"], {
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 63
+          lineNumber: 91
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("br", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 92
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("div", {
+        className: "maps",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 93
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("iframe", {
+        src: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15835.896147127442!2d-34.87990134556883!3d-7.128999696264386!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc1629016d1091cb1!2sCentro+Cultural+Ariano+Suassuna!5e0!3m2!1spt-BR!2sbr!4v1560875453071!5m2!1spt-BR!2sbr",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 94
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("img", {
+        src: "".concat(BACKEND_URL, "/static/img/ct.jpg"),
+        alt: "ariano suassuna",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 97
+        },
+        __self: this
+      })), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("br", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 99
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("br", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 99
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("hr", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 100
+        },
+        __self: this
+      })), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(_components_footer__WEBPACK_IMPORTED_MODULE_13__["default"], {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 102
         },
         __self: this
       }));
@@ -12447,11 +12549,11 @@ __webpack_require__.r(__webpack_exports__);
 var site = {
   title: 'DevOps Conference',
   logo: '/static/img/logos/e.png',
-  dateEventDetail: 'De 29 de Junho a 05 de Julho!',
-  dateEventStart: '06/29/2019 19:00:00',
-  dateEventFinish: '07/05/2019 15:30:00',
-  dateRegistrationStart: '06/01/2019',
-  dateRegistrationFinish: '06/28/2019',
+  dateEventDetail: '17 de Agosto!',
+  dateEventStart: '08/17/2019 08:00:00',
+  dateEventFinish: '08/17/2019 18:30:00',
+  dateRegistrationStart: '06/17/2019',
+  dateRegistrationFinish: '08/17/2019',
   menu: {
     logos: {
       initial: '/static/img/logos/e.png',
@@ -12461,7 +12563,7 @@ var site = {
   about: {
     title: 'Sobre o DevOps Conference'
   },
-  hashTag: '#devopsconference',
+  hashTag: 'devopscf',
   footer: {
     socialsNetwork: [{
       name: 'github',
@@ -12469,7 +12571,35 @@ var site = {
       url: 'https://github.com/devops-conference'
     }],
     copyrigth: '© Todos os direitos reservados, 2019.'
-  }
+  },
+  programacao: [{
+    horario: '09:00 até 09:50',
+    nome: 'O que é DevOps e a Cultura'
+  }, {
+    horario: '09:50 até 10:40',
+    nome: ''
+  }, {
+    horario: '10:40 até 11:30',
+    nome: ''
+  }, {
+    horario: '12:00 até 13:30',
+    nome: 'Almoço'
+  }, {
+    horario: '13:30 até 14:20',
+    nome: ''
+  }, {
+    horario: '14:20 até 15:30',
+    nome: ''
+  }, {
+    horario: '15:30 até 16:00',
+    nome: 'Coffee Break'
+  }, {
+    horario: '16:00 até 16:50',
+    nome: ''
+  }, {
+    horario: '17:00 até 18:00',
+    nome: 'Mesa Redonda'
+  }]
 };
 /* harmony default export */ __webpack_exports__["default"] = (site);
 
