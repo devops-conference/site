22a9ac50c165db5fe3e4619a2df9b59f90f4bfbd
magnonta <magnonta@gmail.com>
2019-06-27 14:51:20 -0300

    Deploy to gh-pages

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ece001fb819c6e71e20c";
+/******/ 	var hotCurrentHash = "e588613300ed02838a67";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
