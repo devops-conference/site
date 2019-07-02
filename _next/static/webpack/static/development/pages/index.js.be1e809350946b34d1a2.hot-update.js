eac340b16b9e8b037e74b831042f8bf3cdaea3e3
magnonta <magnonta@gmail.com>
2019-07-02 17:14:16 -0300

    Deploy to gh-pages

---
+++ _next/static/webpack/static/development/pages/index.js.be1e809350946b34d1a2.hot-update.js
@@ -0,0 +1,125 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./utils/config.js":
+/*!*************************!*\
+  !*** ./utils/config.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+var site = {
+  title: 'DevOps Conference',
+  logo: '/static/img/logos/e.png',
+  dateEventDetail: '17 de Agosto!',
+  dateEventStart: '08/17/2019 08:00:00',
+  dateEventFinish: '08/17/2019 18:30:00',
+  dateRegistrationStart: '06/17/2019',
+  dateRegistrationFinish: '08/17/2019',
+  menu: {
+    logos: {
+      initial: '/static/img/logos/e.png',
+      scrolling: '/static/img/logos/e.png'
+    }
+  },
+  about: {
+    title: 'Sobre o DevOps Conference'
+  },
+  hashTag: 'devopscf',
+  footer: {
+    socialsNetwork: [{
+      name: 'github',
+      icon: '/static/img/icons/github_white.png',
+      url: 'https://github.com/devops-conference'
+    }],
+    copyrigth: '© Todos os direitos reservados, 2019.'
+  },
+  programacao: [{
+    horario: '09:00 até 09:50',
+    nome: 'A Cultura DevOps e sua importância - Kevin Medeiros'
+  }, {
+    horario: '09:50 até 10:40',
+    nome: 'IaaC Infraestructure as a Code - Luiz Thiago'
+  }, {
+    horario: '10:40 até 11:30',
+    nome: 'Kubernetes e Istio para acelerar suas entregas'
+  }, {
+    horario: '12:00 até 13:30',
+    nome: 'Almoço'
+  }, {
+    horario: '13:30 até 14:20',
+    nome: 'Apache Kafka: não é Netflix, mas também tem streaming - Ramon Pereira'
+  }, {
+    horario: '14:20 até 15:30',
+    nome: 'Integração e Entrega Contínua com Jenkins, Git, Docker e Terraform - Aécio'
+  }, {
+    horario: '15:30 até 16:00',
+    nome: 'Networking'
+  }, {
+    horario: '16:00 até 16:50',
+    nome: 'Cloud Native Computing Foundation - Osmar Leão'
+  }, {
+    horario: '17:00 até 18:00',
+    nome: 'Mesa Redonda'
+  }],
+  palestrantes: [{
+    img: '/static/img/k.jpeg',
+    name: 'Kevin Medeiros',
+    profession: 'DevOps Engineer na Anchor'
+  }, {
+    img: '/static/img/victor.jpeg',
+    name: 'Luiz Thiago',
+    profession: 'DevOps Engineer na Reliant Solutions'
+  }, {
+    img: '/static/img/ramon.jpeg',
+    name: 'Ramon Souza',
+    profession: 'Especialista de TI na Conductor'
+  }, {
+    img: '/static/img/aecio.jpeg',
+    name: 'Aecio Pires',
+    profession: 'Analista de Suporte na Unirede'
+  }, {
+    img: '/static/img/gregorio.jpg',
+    name: 'Gregório Melo',
+    profession: 'Desenvolvedor na ThoughWorks'
+  }, {
+    img: '/static/img/osmar.jpg',
+    name: 'Osmar Leão',
+    profession: 'Consultor de produtos da Oracle Brasil'
+  }],
+  patrocinadores: [{
+    img: '/static/img/logo1.png',
+    name: 'Tribunal de Contas do Estado da Paraíba'
+  }, // {
+  //     img: '/static/img/logo2.png',
+  //     name: 'VAT60 - podcast de tecnologia',
+  // },
+  {
+    img: '/static/img/logovsoft.png',
+    name: 'VSOFT Tecnologia'
+  }],
+  organizadores: [{
+    img: '/static/img/magno.jpg',
+    name: 'Magno Oliveira',
+    profession: 'DevOps Engineer na Conductor Tecnologia'
+  }, {
+    img: '/static/img/roberto.jpg',
+    name: 'Roberto Carneiro',
+    profession: 'Gerente de Infraestrutura na Conductor Tecnologia'
+  }, {
+    img: '/static/img/rodrigo.jpg',
+    name: 'Rodrigo Lira',
+    profession: 'Analista de Sistemas Senior na PBSoft/TCE'
+  }, {
+    img: '/static/img/jesse.jpg',
+    name: 'Jessé Neto',
+    profession: 'Analista de suporte de redes Senior na Indra'
+  }]
+};
+/* harmony default export */ __webpack_exports__["default"] = (site);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.be1e809350946b34d1a2.hot-update.js.map
