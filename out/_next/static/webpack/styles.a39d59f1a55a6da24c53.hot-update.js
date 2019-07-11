11687ce7c16dd7663b08ad0fed834a48789e018e
magnonta <magnonta@gmail.com>
2019-07-11 14:00:51 -0300

    Deploy to gh-pages

--- out/_next/static/webpack/styles.a39d59f1a55a6da24c53.hot-update.js
+++ out/_next/static/webpack/styles.a39d59f1a55a6da24c53.hot-update.js
@@ -1,4 +1,4 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["styles"],{
+webpackHotUpdate("styles",{
 
 /***/ "./styles/style.scss":
 /*!***************************!*\
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1562864110240");
+            injectCss(link, link.href.split("?")[0] + "?unix=1562863965663");
           });
       }
     }
@@ -37,5 +37,5 @@
 
 /***/ })
 
-}]);
-//# sourceMappingURL=styles.js.map
+})
+//# sourceMappingURL=styles.a39d59f1a55a6da24c53.hot-update.js.map
